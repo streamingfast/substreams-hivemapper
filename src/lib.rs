dad2b48970d668b3357122b9f2beeea8d902ca30
Eduard Voiculescu <eduardvoiculescu95@gmail.com>
2023-01-18 17:05:32 -0500

    wip

--- src/lib.rs
+++ src/lib.rs
@@ -1,9 +1,15 @@
 mod pb;
+mod instruction;
+mod utils;
+mod option;
 
 use substreams::errors::Error;
+use substreams::log;
+use crate::instruction::TokenInstruction;
 use crate::pb::hivemapper::Holders;
-use crate::pb::spl;
+use crate::pb::{hivemapper, spl};
 
+const TOKEN_PROGRAM: &str = "TokenkegQfeZyiNwAJbNbGKPFXCWuBvf9Ss623VQ5DA";
 const _HONEY_CONTRACT_ADDRESS: &str = "4vMsoUT2BWatFweudnQM1xedRLfJgJ7hswhcpz4xgBTy";
 
 #[substreams::handlers::map]
@@ -14,6 +20,17 @@ pub fn map_holders(block: spl::Block) -> Result<Holders, Error> {
         if let Some(meta) = trx.meta {
             if let Some(_) = meta.err {
                 continue;
+            }
+            if let Some(transaction) = trx.transaction {
+                if let Some(msg) = transaction.message {
+                    for inst in msg.instructions {
+                        let program_id = &msg.account_keys[inst.program_id_index as usize];
+                        if bs58::encode(program_id).into_string() != "TokenkegQfeZyiNwAJbNbGKPFXCWuBvf9Ss623VQ5DA" {
+                            continue;
+                        }
+                        log::info!("program_id {}", bs58::encode(program_id).into_string());
+                    }
+                }
             }
         }
     }
