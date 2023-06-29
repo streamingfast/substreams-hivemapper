a25038354f83ed2c9166d14d1e066484b0365b4e
Eduard Voiculescu <eduardvoiculescu95@gmail.com>
2023-06-29 16:22:48 -0400

    adding map ai trainer rewards

--- src/lib.rs
+++ src/lib.rs
@@ -1,9 +1,10 @@
 mod pb;
 mod utils;
 
-use crate::pb::hivemapper::types::v1::driver_reward::DriverType::{NoSplit, Regular};
+use crate::pb::hivemapper::types::v1::driver_payment::DriverType::{NoSplit, Regular};
 use crate::pb::hivemapper::types::v1::{
-    DriverReward, DriverRewards, Payment, TokenSplittingPayment, TokenSplittingPayments,
+    AiTrainerPayment, AiTrainerPayments, DriverPayment, DriverPayments, Payment, TokenSplittingPayment,
+    TokenSplittingPayments,
 };
 use substreams::errors::Error;
 use substreams::log;
@@ -17,6 +18,7 @@ const HONEY_TOKEN_SPLITTING_INSTRUCTION_PROGRAM: &str = "BNH1dUp3ExFbgo3YctSqQbJ
 const HONEY_TOKEN_SPLITTING_INSTRUCTION_BYTE: u8 = 172; // ac
 const HONEY_NO_TOKEN_SPLITTING_INSTRUCTION_BYTE: u8 = 179; // b3
 const HONEY_REGULAR_DRIVER_INSTRUCTION_BYTE: u8 = 151; // 97
+const HONEY_AI_TRAINER_INSTRUCTION_BYTE: u8 = 241; // f1
 const HONEY_TOKEN_DECIMALS: u8 = 9;
 
 #[substreams::handlers::map]
@@ -40,10 +42,10 @@ pub fn map_token_splitting_payments(block: Block) -> Result<TokenSplittingPaymen
                         continue;
                     }
 
-                    // let token_account = bs58::encode(&msg.account_keys[inst.accounts[1] as usize]).into_string();
-                    // if token_account != HONEY_CONTRACT_ADDRESS {
-                    //     continue;
-                    // }
+                    let token_account = bs58::encode(&msg.account_keys[inst.accounts[1] as usize]).into_string();
+                    if token_account != HONEY_CONTRACT_ADDRESS {
+                        continue;
+                    }
 
                     if inst.data[0] == HONEY_TOKEN_SPLITTING_INSTRUCTION_BYTE {
                         // ac: instruction for token splitting
@@ -62,17 +64,6 @@ pub fn map_token_splitting_payments(block: Block) -> Result<TokenSplittingPaymen
 
                 if let Some(_) = msg.header {
                     let meta = confirmed_trx.meta.as_ref().unwrap();
-
-                    if meta.inner_instructions.len() != 1 as usize {
-                        // seems to work for now, not sure if its good for all transactions
-                        continue;
-                    }
-                    if meta.inner_instructions[0].instructions.len() != 4 as usize {
-                        // seems to work for now, not sure if its good for all transactions
-                        // what about a splitting contract where we have an owner with 100%
-                        continue;
-                    }
-
                     let mut manager_payment = Payment::default();
                     let mut driver_payment = Payment::default();
 
@@ -94,7 +85,6 @@ pub fn map_token_splitting_payments(block: Block) -> Result<TokenSplittingPaymen
                                         manager_payment.timestamp = timestamp;
                                         manager_payment.transaction_id = trx_id.clone();
                                         manager_payment.address = account_to;
-                                        manager_payment.address_owner = "todo".to_string();
                                         manager_payment.amount =
                                             utils::amount_to_decimals(amount as f64, HONEY_TOKEN_DECIMALS as f64);
                                         continue;
@@ -104,7 +94,6 @@ pub fn map_token_splitting_payments(block: Block) -> Result<TokenSplittingPaymen
                                         driver_payment.timestamp = timestamp;
                                         driver_payment.transaction_id = trx_id.clone();
                                         driver_payment.address = account_to;
-                                        driver_payment.address_owner = "todo".to_string();
                                         driver_payment.amount =
                                             utils::amount_to_decimals(amount as f64, HONEY_TOKEN_DECIMALS as f64);
                                     }
@@ -127,7 +116,7 @@ pub fn map_token_splitting_payments(block: Block) -> Result<TokenSplittingPaymen
 }
 
 #[substreams::handlers::map]
-pub fn map_driver_payments(block: Block) -> Result<DriverRewards, Error> {
+pub fn map_driver_payments(block: Block) -> Result<DriverPayments, Error> {
     let mut payments = vec![];
     let timestamp = block.block_time.as_ref().unwrap().timestamp;
 
@@ -144,6 +133,11 @@ pub fn map_driver_payments(block: Block) -> Result<DriverRewards, Error> {
                         bs58::encode(&msg.account_keys[inst.program_id_index as usize]).into_string();
 
                     if instruction_program_account != HONEY_TOKEN_SPLITTING_INSTRUCTION_PROGRAM {
+                        continue;
+                    }
+
+                    let token_account = bs58::encode(&msg.account_keys[inst.accounts[1] as usize]).into_string();
+                    if token_account != HONEY_CONTRACT_ADDRESS {
                         continue;
                     }
 
@@ -186,7 +180,6 @@ pub fn map_driver_payments(block: Block) -> Result<DriverRewards, Error> {
                                         driver_payment.timestamp = timestamp;
                                         driver_payment.transaction_id = trx_id.clone();
                                         driver_payment.address = account_to;
-                                        driver_payment.address_owner = "todo".to_string();
                                         driver_payment.amount =
                                             utils::amount_to_decimals(amount as f64, HONEY_TOKEN_DECIMALS as f64);
                                     }
@@ -196,7 +189,7 @@ pub fn map_driver_payments(block: Block) -> Result<DriverRewards, Error> {
                         }
                     }
 
-                    let mut dr = DriverReward {
+                    let mut dr = DriverPayment {
                         payment: Some(driver_payment),
                         r#type: 0,
                     };
@@ -215,5 +208,83 @@ pub fn map_driver_payments(block: Block) -> Result<DriverRewards, Error> {
         }
     }
 
-    Ok(DriverRewards { rewards: payments })
+    Ok(DriverPayments { rewards: payments })
+}
+
+#[substreams::handlers::map]
+pub fn map_ai_trainer_payments(block: Block) -> Result<AiTrainerPayments, Error> {
+    let mut payments = vec![];
+    let timestamp = block.block_time.as_ref().unwrap().timestamp;
+
+    for confirmed_trx in block.transactions().filter(|trx| trx.meta().is_some()) {
+        if let Some(trx) = &confirmed_trx.transaction {
+            let trx_id = bs58::encode(&trx.signatures[0]).into_string();
+            if let Some(msg) = &trx.message {
+                let mut honey_ai_trainer_payment = false;
+                let mut driver_account = String::new();
+
+                for inst in msg.instructions.iter() {
+                    let instruction_program_account =
+                        bs58::encode(&msg.account_keys[inst.program_id_index as usize]).into_string();
+
+                    if instruction_program_account != HONEY_TOKEN_SPLITTING_CONTRACT {
+                        continue;
+                    }
+
+                    let token_account = bs58::encode(&msg.account_keys[inst.accounts[1] as usize]).into_string();
+                    if token_account != HONEY_CONTRACT_ADDRESS {
+                        continue;
+                    }
+
+                    if inst.data[0] == HONEY_AI_TRAINER_INSTRUCTION_BYTE {
+                        honey_ai_trainer_payment = true;
+                        driver_account = bs58::encode(&msg.account_keys[inst.accounts[2] as usize]).into_string();
+                        break;
+                    }
+                }
+
+                if !honey_ai_trainer_payment {
+                    continue;
+                }
+
+                if let Some(_) = msg.header {
+                    let meta = confirmed_trx.meta.as_ref().unwrap();
+                    let mut driver_payment = Payment::default();
+
+                    for inner_inst in meta.inner_instructions.iter() {
+                        for inst in inner_inst.instructions.iter() {
+                            let program_id = &msg.account_keys[inst.program_id_index as usize];
+                            let account_id = bs58::encode(program_id).into_string();
+
+                            if account_id != TOKEN_PROGRAM {
+                                continue;
+                            }
+
+                            let account_to = utils::fetch_account_to(&msg.account_keys, inst.accounts[1]);
+
+                            let instruction = TokenInstruction::unpack(&inst.data)?;
+                            match instruction {
+                                TokenInstruction::MintTo { amount } => {
+                                    if account_to == driver_account {
+                                        driver_payment.timestamp = timestamp;
+                                        driver_payment.transaction_id = trx_id.clone();
+                                        driver_payment.address = account_to;
+                                        driver_payment.amount =
+                                            utils::amount_to_decimals(amount as f64, HONEY_TOKEN_DECIMALS as f64);
+                                    }
+                                }
+                                _ => {}
+                            }
+                        }
+                    }
+
+                    payments.push(AiTrainerPayment {
+                        payment: Some(driver_payment),
+                    });
+                }
+            }
+        }
+    }
+
+    Ok(AiTrainerPayments { payments })
 }
