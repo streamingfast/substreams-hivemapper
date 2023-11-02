c5d8891ee16c8054e8cf6fa81a16f56d5632303f
Eduard Voiculescu <eduardvoiculescu95@gmail.com>
2023-11-02 16:27:11 -0400

    update with resolved accounts

--- src/lib.rs
+++ src/lib.rs
@@ -15,15 +15,13 @@ pub fn map_outputs(block: Block) -> Result<Output, Error> {
     let timestamp = block.block_time.as_ref().unwrap().timestamp;
 
     for confirmed_trx in block.transactions_owned() {
+        let accounts = confirmed_trx.resolved_accounts_as_strings();
+
         if let Some(trx) = confirmed_trx.transaction {
             let trx_hash = bs58::encode(&trx.signatures[0]).into_string();
             let msg = trx.message.unwrap();
-            let mut accounts = vec![];
-            msg.account_keys
-                .into_iter()
-                .for_each(|addr| accounts.push(bs58::encode(addr).into_string()));
-
             let meta = confirmed_trx.meta.as_ref().unwrap();
+
             for (i, compiled_instruction) in msg.instructions.iter().enumerate() {
                 utils::process_compiled_instruction(
                     &mut output,
